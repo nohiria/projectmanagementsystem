b249871295c771496f242c526df5400bd3ec70bf
Nohiria Velazquez <nohiriavgvg@gmail.com>
2024-12-30 14:51:57 -0500

    Jwt configuration

---
+++ src/main/java/com/nohiria/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.nohiria.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "sjanjbdifbiweb fwjbeifubwiefb wiebfiwbef";
+    public static final String JWT_HEADER = "Authorization";
+}
